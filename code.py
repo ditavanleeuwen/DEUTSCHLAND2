3ab40685270a80840a2851f91930a5da932100fd
ditavanleeuwen <117078034+ditavanleeuwen@users.noreply.github.com>
2025-12-02 16:07:19 +0100

    test code

--- code.py
+++ code.py
@@ -1,1 +1,8 @@
 #test
+import numpy as np # linear algebra
+import pandas as pd # data processing
+import re
+from transformers import pipeline
+
+movies = pd.read_csv("movie_test.csv")
+movies.head()
